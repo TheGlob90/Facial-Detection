d080bec8a034548d5e3e629b78423a109dfea7b2
Brandon Lefert <brandon.lefert@gmail.com>
2023-01-22 15:15:22 -0600

    Moved everything to run through the GUI.

--- Facial_Detection/face_training.py
+++ Facial_Detection/face_training.py
@@ -2,11 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
-# Path for face image database
-path = 'dataset'
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
-# function to get the images and label data
+
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
@@ -20,10 +16,17 @@ def getImagesAndLabels(path):
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
-print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces,ids = getImagesAndLabels(path)
-recognizer.train(faces, np.array(ids))
-# Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') 
-# Print the numer of faces trained and end program
-print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+def main():
+    # Path for face image database
+    path = 'dataset'
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+    # function to get the images and label data
+    print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+    faces,ids = getImagesAndLabels(path)
+    recognizer.train(faces, np.array(ids))
+    # Save the model into trainer/trainer.yml
+    recognizer.write('trainer/trainer.yml') 
+    # Print the numer of faces trained and end program
+    print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
