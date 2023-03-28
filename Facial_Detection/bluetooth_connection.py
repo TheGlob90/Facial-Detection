15b88f07453ff0e95d810a8c8b6ecce51635a108
Brandon Lefert <brandon.lefert@gmail.com>
2023-03-28 15:10:38 -0500

    Test2

--- Facial_Detection/bluetooth_connection.py
+++ Facial_Detection/bluetooth_connection.py
@@ -39,7 +39,7 @@ class WorkerBLE(QRunnable):
         self.signals.signalMsg.emit("WorkerBLE start")
         
         #---------------------------------------------
-        p = btle.Peripheral("3c:71:bf:0d:dd:6a")
+        p = btle.Peripheral("54:43:B2:2B:A3:E0")
         p.setDelegate( MyDelegate(self.signals) )
  
         svc = p.getServiceByUUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
