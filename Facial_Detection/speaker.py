b04c2393f004642db1246c94cd3f7f914134f8ee
Brandon Lefert <brandon.lefert@gmail.com>
2023-04-19 11:09:52 -0500

    Added timer to speaker for testing

--- Facial_Detection/speaker.py
+++ Facial_Detection/speaker.py
@@ -3,7 +3,8 @@ import board
 
 def speaker(): 
     pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=880)
-    while True:
+    count = 0
+    while count < 100:
         time.sleep(0.1)
         pwm.frequency = 500
         time.sleep(0.1)
